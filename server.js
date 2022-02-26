783e9a51cba2c66848df99d8de599e01f989fd86
G B Aswath <gbaswath@zohomail.in>
2022-02-26 07:14:04 +0530

    Timestamp Microservice
    1. .gitignore -> skipped .env file
    2. package.json, package-lock.json -> Added DotEnv package to load port from environment variable.
    3. server.js -> Use dotenv to load port, added /api/:date to handle date and prepare expected response

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
+
+//Load Environment Variables
+require('dotenv').config();
+console.log("Environment Variables " + JSON.stringify(process.env));
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,10 +25,34 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
-
+//Return Unix Time & UTC String Date
+app.get("/api/:date", function (req, res) {
+  //Get Date from URL
+  let reqDate = req.params['date'];
+  console.log("Requested Date " + reqDate + " from URL");
+  //Parse Create Date
+  let date = new Date(reqDate);
+  //If Date is unix Time format it needs to handled separately
+  if(isNaN(date)) {
+    console.log("Date is not in String Format");
+    date = new Date(parseInt(reqDate));
+    console.log("Date is in Unix Time Format " + date);
+  }
+  //Parsed Date Result
+  console.log("Parsed Date " + date);
+  //Convert Date to Unix Time
+  let unixTime = date.getTime();
+  console.log("Unix Time " + unixTime);
+  //Convert Date to UTC String Format
+  let utcDate = date.toUTCString();
+  console.log("UTC String " + utcDate);
+  //Prepare JSON Result
+  let result = {"unix": unixTime, "utc" : utcDate};
+  res.json(result);
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
